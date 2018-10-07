3a5db15ee6a38b5df13d0f5d1676da56978d8b49
Joshua Qualls <jrqualls@iu.edu>
2018-10-07 19:51:46 -0400

    Add files via upload

---
+++ proficiency-01/AndroidProficency01.java
@@ -0,0 +1,39 @@
+package com.example.joshu.capstone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.sql.Timestamp;
+
+public class AndroidProficency01 extends AppCompatActivity {
+    TextView tv = (TextView)findViewById(R.id.test);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_proficency01);
+
+        Long tsLong = System.currentTimeMillis();
+        Timestamp t = new Timestamp(tsLong);
+
+        String tsString = t.toString();
+        String sHour = tsString.substring(11, 13);
+        int hour = Integer.parseInt(sHour);
+
+
+        if (hour >= 6 && hour < 12) {
+            tv.setText("Good Morning!");
+        } else if (hour >= 12 && hour < 19) {
+            tv.setText("Good Afternoon!");
+        } else {
+            tv.setText("Good Evening!");
+        }
+    }
+
+}
+
+
+
+
